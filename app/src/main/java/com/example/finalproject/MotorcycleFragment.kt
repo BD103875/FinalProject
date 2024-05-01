e735b65cddf9553d80ce60498dfa7ae2ef99a050
BD103875 <bryondickson@icloud.com>
2024-04-30 23:04:56 -0400

    Added Splash Screen (unfunctional) -Needs more work to be finished, AppCompatActivity() is causing issues letting the theme be my created animation.

--- app/src/main/java/com/example/finalproject/MotorcycleFragment.kt
+++ app/src/main/java/com/example/finalproject/MotorcycleFragment.kt
@@ -1,23 +1,45 @@
 package com.example.finalproject
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.finalproject.databinding.FragmentMainBinding
+import android.widget.Toast
+import com.example.finalproject.databinding.FragmentMotorcycleBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class MotorcycleFragment : Fragment() {
-    private var _binding: FragmentMainBinding? = null
+    private var _binding: FragmentMotorcycleBinding? = null
     private val binding get() = _binding!!
+
+    val motorcycleMakeArray = listOf("Harley-Davidson", "Kawasaki", "Honda", "BMW", "Yamaha", "KTM", "Ducati", "Aprilia", "Suzuki", "Bimota", "Royal Enfield", "Indian", "Alta", "Energica")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        _binding = FragmentMotorcycleBinding.inflate(inflater, container, false)
 
+        binding.searchButton.setOnClickListener{view ->
+            if(TextUtils.isEmpty(binding.makeText.text.toString())){
+                Snackbar.make(binding.myCoordinatorLayout, R.string.promptUser, Snackbar.LENGTH_SHORT).show()
+            }
+            fun makeMatch(): Boolean {
+                for (motorcycleMakes in motorcycleMakeArray) {
+                    if (binding.makeText.text.toString() == motorcycleMakes) {
+                         return true
+                    }
+                }
+                return false
+            }
+            if (!(makeMatch())) {
+                Toast.makeText(context, R.string.enterValidMakeToast, Toast.LENGTH_SHORT).show()
+            }
+
+    }
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/finalproject/SplashScreenViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.finalproject
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class SplashScreenViewModel : ViewModel() {
+    private val _isReady = MutableStateFlow(false)
+    val isReady = _isReady.asStateFlow()
+
+
+    init {
+        viewModelScope.launch {
+            delay(3000L)
+            _isReady.value = true
+        }
+    }
+
+
+}
