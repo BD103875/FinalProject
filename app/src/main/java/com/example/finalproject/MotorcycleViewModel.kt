3dcf0cb9ffab1eac7bb5b8beaa854650dc3f9016
BD103875 <bryondickson@icloud.com>
2024-05-06 13:00:08 -0400

    Minor bug fixes

--- app/src/main/java/com/example/finalproject/MotorcycleViewModel.kt
+++ app/src/main/java/com/example/finalproject/MotorcycleViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.finalproject
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MotorcycleViewModel : ViewModel() {
     val message = "Need help finding your make?"
+
 }
