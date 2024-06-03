b72a6a892b4c009f62a6cde4728ada18e4d748d9
BD103875 <bryondickson@icloud.com>
2024-06-03 13:43:50 -0400

    Complete Final Project

--- app/src/main/java/com/example/finalproject/MotorcycleApiInterface.kt
+++ app/src/main/java/com/example/finalproject/MotorcycleApiInterface.kt
@@ -1,4 +1,5 @@
 package com.example.finalproject
+
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.http.GET
@@ -11,16 +12,24 @@ import retrofit2.http.Query
 interface MotorcycleApiInterface {
     @Headers("X-Api-Key: uBejY3WJOIr1rrgMBJ93IA==HjIWcQI0em8p6CGs")
     @GET("motorcycles")
-    fun getMotorcycles(@Query("make") make:String, @Query("model") model: String, @Query("year") year: String) : Call<List<MotorcycleFeatures>>
+    fun getMotorcycles(
+        @Query("make") make: String,
+        @Query("model") model: String,
+        @Query("year") year: String
+    ): Call<List<MotorcycleFeatures>>
 }
+
 private const val BASE_URL = "https://api.api-ninjas.com/v1/"
 
 private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-private val retrofitObject = Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(
-    moshi)).baseUrl(BASE_URL).build()
+private val retrofitObject = Retrofit.Builder().addConverterFactory(
+    MoshiConverterFactory.create(
+        moshi
+    )
+).baseUrl(BASE_URL).build()
 
-object MotorcycleApiObject{
-    val motorcycleApiObject : MotorcycleApiInterface by lazy {
+object MotorcycleApiObject {
+    val motorcycleApiObject: MotorcycleApiInterface by lazy {
         retrofitObject.create(MotorcycleApiInterface::class.java)
     }
 }
